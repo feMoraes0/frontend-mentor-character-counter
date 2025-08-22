52b180b3f92e8c9cfcfd3f7b68ff775cedff7956
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-22 20:51:15 +0100

    chore(vitest): configures with vitest and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,4 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    // https://github.com/vitest-dev/vitest/blob/main/examples/react/vitest.config.ts
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setupTests.js',
+    css: true,
+  },
 });
