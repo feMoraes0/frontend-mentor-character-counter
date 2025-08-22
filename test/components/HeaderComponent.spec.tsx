81fd8cf2a31cb17bf2bad369b2ca92f40e64ab0a
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-22 20:54:48 +0100

    test(theme-switch): implements with localStorage and browser support to initial state

---
+++ test/components/HeaderComponent.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import HeaderComponent from '../../src/components/HeaderComponent';
+
+describe('Given the Header Component', () => {
+  it('When theme button is clicked, then should validate dark to light theme switch', async () => {
+    const user = userEvent.setup();
+    render(<HeaderComponent />);
+    const buttonTheme = screen.getByRole('button', {
+      name: /dark theme/i,
+    });
+    expect(buttonTheme).toBeInTheDocument();
+    await user.click(buttonTheme);
+    const darkButtonTheme = screen.queryByRole('button', {
+      name: /dark theme/i,
+    });
+    expect(darkButtonTheme).not.toBeInTheDocument();
+    const lightButtonTheme = screen.getByRole('button', {
+      name: /light theme/i,
+    });
+    expect(lightButtonTheme).toBeInTheDocument();
+  });
+});
